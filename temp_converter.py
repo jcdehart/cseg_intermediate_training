65dc0ed6edc316c553abc361837786293a190335
Jen DeHart <jcdehart@colostate.edu>
2022-04-28 14:04:46 -0600

    C to K

--- temp_converter.py
+++ temp_converter.py
@@ -63,8 +63,8 @@ def fahrenheit_to_celcius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
 
-def place_holder():
-    pass
+def celcius_to_kelvin():
+    return (celcius + 273.25)
 
 
 if __name__ == "__main__":
